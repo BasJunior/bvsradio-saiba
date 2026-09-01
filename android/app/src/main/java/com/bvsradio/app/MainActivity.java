da4f3e8963a8ecffdf1ef1c36f335aab144d513d
BasJunior <83550884+BasJunior@users.noreply.github.com>
2026-09-02 00:00:01 +0200

    feat(app-vnext): register Android offline media bridge

--- android/app/src/main/java/com/bvsradio/app/MainActivity.java
+++ android/app/src/main/java/com/bvsradio/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.bvsradio.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BvsOfflineMediaPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
