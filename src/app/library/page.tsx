efaae195da790546f6432b3b9f17487cbefa8ed9
BasJunior <83550884+BasJunior@users.noreply.github.com>
2026-09-07 17:07:10 +0200

    refactor(web): make Library a single coherent surface

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,9 +1,5 @@
 import LibraryView from "@/components/library/LibraryView";
-import WebPlaylists from "@/components/library/WebPlaylists";
 
 export default function LibraryPage() {
-  return <>
-    <LibraryView />
-    <WebPlaylists />
-  </>;
+  return <LibraryView />;
 }
