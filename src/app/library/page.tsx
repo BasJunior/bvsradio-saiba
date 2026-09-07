284a031e9e2eb03407ebf30ad73187ba4d824bec
BasJunior <83550884+BasJunior@users.noreply.github.com>
2026-09-07 19:45:45 +0200

    feat(web): make lyrics pad easy to find

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,5 +1,9 @@
 import LibraryView from "@/components/library/LibraryView";
+import LibraryLyricsShortcut from "@/components/library/LibraryLyricsShortcut";
 
 export default function LibraryPage() {
-  return <LibraryView />;
+  return <>
+    <LibraryLyricsShortcut />
+    <LibraryView />
+  </>;
 }
