5096ae5d42303744ce51be578e7172606969d0fb
BasJunior <83550884+BasJunior@users.noreply.github.com>
2026-09-08 18:40:18 +0200

    feat(editorial): expose catalogue normalization workspace

---
+++ src/app/editorial/catalogue/page.tsx
@@ -0,0 +1,7 @@
+import EditorialCatalogueNormalizer from '@/components/editorial/EditorialCatalogueNormalizer'
+
+export const dynamic = 'force-dynamic'
+
+export default function EditorialCataloguePage() {
+  return <EditorialCatalogueNormalizer />
+}
